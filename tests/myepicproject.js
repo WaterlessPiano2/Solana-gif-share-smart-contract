ecc627a2f332736f406c7e57252c4465f8612d66
WaterlessPiano2 <scderya@gmail.com>
2023-01-22 23:48:00 +0000

    havean account to store a number uin our contract and fetch the number

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,35 @@
 const anchor = require('@project-serum/anchor');
 
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-    const tx = await program.methods.initialize().rpc();
+	
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
@@ -21,17 +43,3 @@ const runMain = async () => {
 };
 
 runMain();
-
-
-
-describe("myepicproject", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Myepicproject;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
