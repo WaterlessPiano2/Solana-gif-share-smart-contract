6a937ea5f9d07da99ce7a088589c19c515968991
WaterlessPiano2 <scderya@gmail.com>
2023-01-22 22:43:07 +0000

    able to deploy the default progrram on to network and call it using js

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,4 +1,28 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
+
+const main = async() => {
+  console.log("🚀 Starting test...")
+
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.Myepicproject;
+    const tx = await program.methods.initialize().rpc();
+
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+
 
 describe("myepicproject", () => {
   // Configure the client to use the local cluster.
